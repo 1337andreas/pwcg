12fb7c604fce6f35c2c056701a4990f7c4e6e817
Andreas Andersson <andreas.andersson00@hotmail.com>
2026-01-01 13:35:02 +0100

    Version 0.7b
    Feature branch - Online check against HIBP's API. Explained in the script. Currently only supports existing passwords; if generating a new password, you will have to type it in manually in the checker at the moment. The input is also in plaintext for the time being. However, the check does work!

---
+++ PWCG2000_onlinecheck.py
@@ -0,0 +1,126 @@
+#Welcome to PWCG2000!
+#This script can both generate a secure password and check an existing one against a list of millions of known leaked passwords.
+#Furthermore, the user may upon generating a new password choose the amount of symbols needed. 
+#The script will automatically throw in upper- and lowercase letters from A to Z, as well as numbers and special symbols. The generated password also gets cross-checked for leaks to verify its safety.
+#Version 0.7b
+
+import secrets
+import string                                                                                                   #Imports the necessary modules containing random generation and all necessary characters.
+import urllib.request                                                                                           #This library is used for requesting data from HaveIBeenPwnd (HIBP). Importing the "requests" library would also work, but that one is external and would have to be installed then.
+import hashlib                                                                                                  #Importing this is necessary for checking passwords online, as the database API only takes hashes rather than cleartext. From a security standpoint, this is a good thing.
+
+
+                                                                                                                #Variable "userchoice" gets defined by the user's input and makes the script do different things. The while loop ensures the menu shows again after completing a chosen task.
+while True:
+    print("""WELCOME TO
+       ____ _       _________________   ____  ____  ____ 
+      / __ \ |     / / ____/ ____/__ \ / __ \/ __ \/ __ \ 
+     / /_/ / | /| / / /   / / __ __/ // / / / / / / / / /
+    / ____/| |/ |/ / /___/ /_/ // __// /_/ / /_/ / /_/ / 
+   /_/     |__/|__/\____/\____//____/\____/\____/\____/ !
+        
+        MAKE A CHOICE:
+        -(C)heck existing password against HIBP database
+        -(G)enerate a new password
+        -(L)anguage selection (yet to be implemented)
+        -E(x)it""")                                                                                                     #Note that language selection will be in a later feature branch.
+
+    userchoice=input()
+    if userchoice == "C" or userchoice == "c":
+        print("Please enter the password you would like to check.")
+        existing_password = input()                                                                                     #The password input currently shows in plaintext, which should only be in the RAM whilst the code runs. However, some terminals might save your output so use this with caution. I aim to hide this with asterisks or something later.
+
+                                                                                                                        #This function checks your existing password against the HIBP database.
+        def check_password(password: str) -> int:                                                                                                  
+            sha1 = hashlib.sha1(password.encode("utf-8")).hexdigest().upper()                                           #This hashes the password in SHA-1 in order to make the API accept it. The HIBP API does not accept passwords in plaintext, only hashes.
+            prefix = sha1[:5]                                                                                           #Splits the hash value into two variables: Prefix (for the HIPB API) and suffix (for cross-reference). Only the first 5 characters in the hash key are sent to the API. The exact password in itself does therefore not get checked, but rather each password with the same prefix. 
+            suffix = sha1[5:]
+            url = f"https://api.pwnedpasswords.com/range/{prefix}"                                                      #URL to the API. The "prefix" variable in brackets will be different each time the code is run and is defined by the generated hash.
+                                                                                                                
+            req = urllib.request.Request(                                                                               #This makes a HTTP request with User-Agent, which is not necessary but recommended by the API to avoid error 400/403.
+                url,
+                headers={"User-Agent": "PWCG2000"}
+            )
+
+            with urllib.request.urlopen(req, timeout=10) as response:                                                   #Then this reads the data returned by the API, using the "urllib.request" library imported by the script.
+                data = response.read().decode("utf-8")                                                                  #API responds with a line of text that gets converted back.
+
+            for line in data.splitlines():                                                                      
+                hash_suffix, count = line.split(":")                                                                    #This splits the text into different suffixes which are checked.
+                if hash_suffix == suffix:                                                                               #If there is a suffix match, it means the password is in a leak known by HIBP.
+                    return int(count)                                                                                   #No suffix match means the password is not in any known leaks. "return 0" is in case no match is found during the loop.
+
+            return 0
+
+        count = check_password(existing_password)                                                                       #This calls the function and prints the check result.
+
+        if count > 0:
+            print(f"Password was found in {count} leaks!")
+        else:
+            print("Password was not found to be in any known leaks.")
+
+        input("Press RETURN to continue.")                                                                              #Takes the user back to the menu.
+
+
+                                                                                                                        #The "or" statements prevent returning "Invalid choice!" should the user write in lowercase.
+    elif userchoice=="G" or userchoice=="g":
+        try:                                                                                                            #Try/Except ensure this whole section gets skipped should newpassword_length not be an integer.         
+            print ("Please enter the amount of symbols (8-16) you want for the new password.") 
+
+            newpassword_length=int(input("Password length: "))
+            if newpassword_length < 8:
+                input("Password is too short! Press RETURN to continue.")
+            elif newpassword_length > 16:
+                input("Password is too long! Press RETURN to continue.") 
+
+            elif (newpassword_length > 7) and (newpassword_length < 17):                                               #The password generation logic. Program continues here if a valid length (8-16 characters) was chosen.
+                print ("Generating password...")
+                newpassword_characters =""
+                newpassword_characters += string.punctuation                                                           #Punctuation such as commas and exclamation marks.
+                newpassword_characters += string.ascii_letters                                                         #Letters A-Z and a-z
+                newpassword_characters += string.digits                                                                #Numbers 0-9
+                newpassword = []
+                for i in range(newpassword_length):                                                                    #This checks the amount of letters chosen by the user.
+                    randomchar = secrets.choice(newpassword_characters)                                                #This selects a random set of characters out of the ones defined above.
+                    newpassword.append(randomchar)
+                print("Secure password suggestion: " + "".join(newpassword))                                           #The check will be performed here once that feature is implemented.
+                input("Press RETURN to go back to the menu.")
+        except:
+            input("Invalid input! Press RETURN to continue.")
+            
+    elif userchoice=="L" or userchoice=="l":
+        print("""       SELECT A LANGUAGE
+              Svenska
+              English
+              Deutsch""")                                                                                              #Language choice logic will be placed here. Not yet implemented.
+        input("Press RETURN to go back.")
+
+    elif userchoice=="X" or userchoice=="x":
+        break                                                                                                          #This exits the program.
+            
+    else: print ("Invalid choice! Please press one of the keys asked.")                                                #Prevents the program from crashing.
+
+            #Add the different features here
+            #Make a log file!
+            
+            #Changelog 01-01-2026 (v0.7b - HIPB branch)
+            #Added the "check_password" function to check an existing password against HaveIBeenPwnd's API using a SHA-1 hash.
+            #Importing standard libraries which should work on different operating systems. Tested on Windows.
+            #Upon checking a password, the script returns the number of known leaks the password was found in, including when there are none.
+            #Password input currently shows in plaintext, potential security issue that will get fixed.
+
+            #Changelog 01-01-2026 (v0.6)
+            #Importing function "secrets" instead of "random", for a better generation algorithm based on what OS you have.
+            #Try/Except added to the generation logic to prevent crashes when the password length variable isn't defined as an integer.
+            #Moved the comments to improve code readability.
+            #Added this very list and one explaining the variables to help developers keep track of what's what.
+            #The user can now return to the menu without having to run the script again, with a while loop. Waits for user input to continue.
+            #Language selection menu has been added in order to template the script's structure, but the feature itself has yet to be implemented.
+
+            #Variables:
+            #userchoice - String that acts as the user's choice in the menu.
+            #existing_password - String for the user's current password to get checked against known leaks.
+            #newpassword - The new password that the program generates, with certain characteristics.
+            #newpassword_length - Integer defining the amount of characters in the generated new password. User chooses this.
+            #newpassword_characters - String containing the character set that gets used in the password. Defined by the imported ASCII character set; A-Z, a-z, 0-9, punctuation.
+            
