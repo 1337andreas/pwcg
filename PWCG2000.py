99d0cccbce15fdaa79b2bdcd91b47c31eb038938
Andreas Andersson <andreas.andersson00@hotmail.com>
2026-01-02 20:18:57 +0100

    Version 1.0e
    Fixed the password generation which was previously broken when when I made the interface text in separate language files instead of being hard-coded.
    Also added Finnish as an option in the language menu, thank you /saabismi for the translation!
    Further changes are noted in the script.

--- PWCG2000.py
+++ PWCG2000.py
@@ -2,8 +2,9 @@
 #This script can both generate a secure password and check an existing one against a list of millions of known leaked passwords.
 #Furthermore, the user may upon generating a new password choose the amount of symbols needed. 
 #The script will automatically throw in upper- and lowercase letters from A to Z, as well as numbers and special symbols. The generated password also gets cross-checked for leaks to verify its safety.
-#Version 0.8b
+#Version 1.0e - EXPERIMENTAL BRANCH with LOCALISATION
 
+import json                                                                                                     #Required dependency for the localisation feature. More languages could easily be added.
 import getpass
 import secrets
 import string                                                                                                   #Imports the necessary modules containing random generation and all necessary characters.
@@ -12,23 +13,37 @@ import hashlib                                                                  
 
 
                                                                                                                 #Variable "userchoice" gets defined by the user's input and makes the script do different things. The while loop ensures the menu shows again after completing a chosen task.
+
+class Localisation:
+    def __init__(self, language="en"):
+        with open(f"{language}.json", "r", encoding="utf-8") as f:
+            self.texts = json.load(f)
+
+    def t(self, key, **kwargs):
+        text = self.texts.get(key, f"[{key}]")
+        return text.format(**kwargs)
+
+translator = Localisation("en")                                                                                        #Chooses English automatically. Note that this script has dependencies, namely "lang_en.json", "lang_sv.json" and so on.
+t = translator.t
+
 while True:
-    print("""WELCOME TO
+    print(t("menu_title"))
+    print(f"""
        ____ _       _________________   ____  ____  ____ 
       / __ \ |     / / ____/ ____/__ \ / __ \/ __ \/ __ \ 
      / /_/ / | /| / / /   / / __ __/ // / / / / / / / / /
     / ____/| |/ |/ / /___/ /_/ // __// /_/ / /_/ / /_/ / 
    /_/     |__/|__/\____/\____//____/\____/\____/\____/ !
         
-        MAKE A CHOICE:
-        -(C)heck existing password against HIBP database
-        -(G)enerate a new password
-        -(L)anguage selection (yet to be implemented)
-        -E(x)it""")                                                                                                     #Note that language selection will be in a later feature branch.
+            {t("menu_choice")}
+            {t("menu_checker")}
+            {t("menu_generator")}
+            {t("menu_language")}
+            {t("menu_exit")}""")                                                                                        #Instead of having the user instructions directly in the script, it reads from the chosen language JSON file.
 
     userchoice=input()
-    if userchoice == "C" or userchoice == "c":
-        print("Please enter the password you would like to check.")
+    if userchoice == "C" or userchoice == "c" or userchoice == "K" or userchoice == "k" or userchoice == "T" or userchoice == "t":  #The multiple OR conditions in this IF statement are used for Swedish and German.
+        print(t("enter_password_check"))
         existing_password = getpass.getpass()                                                                           #Using "getpass" instead of "input" hides the password from shoulder-peeking. The password should only be in the RAM whilst the code runs. However, some terminals might save your input so putting sensitive information in here happens at your own risk.
                                                                                                                         #This function checks your existing password against the HIBP database.
         def check_password(password: str) -> int:                                                                                                  
@@ -55,26 +70,25 @@ while True:
         count = check_password(existing_password)                                                                       #This calls the function and prints the check result.
 
         if count > 0:
-            print(f"Password was found in {count} leaks!")
+            print(t("password_found", count=count))
         else:
-            print("Password was not found to be in any known leaks.")
+            print(t("password_safe"))
 
-        input("Press RETURN to continue.")                                                                              #Takes the user back to the menu.
+        input(t("press_return"))                                                                                        #Takes the user back to the menu.
 
 
                                                                                                                         #The "or" statements prevent returning "Invalid choice!" should the user write in lowercase.
     elif userchoice=="G" or userchoice=="g":
         try:                                                                                                            #Try/Except ensure this whole section gets skipped should newpassword_length not be an integer.         
-            print ("Please enter the amount of symbols (8-16) you want for the new password.") 
+            print (t("enter_password_length")) 
 
-            newpassword_length=int(input("Password length: "))
+            newpassword_length=int(input(t("password_length_prompt")))
             if newpassword_length < 8:
-                input("Password is too short! Press RETURN to continue.")
+                input(t("password_too_short"))
             elif newpassword_length > 16:
-                input("Password is too long! Press RETURN to continue.") 
-
+                input(t("password_too_long")) 
             elif (newpassword_length > 7) and (newpassword_length < 17):                                               #The password generation logic. Program continues here if a valid length (8-16 characters) was chosen.
-                print ("Generating password...")
+                print (t("generating_password"))
                 newpassword_characters =""
                 newpassword_characters += string.punctuation                                                           #Punctuation such as commas and exclamation marks.
                 newpassword_characters += string.ascii_letters                                                         #Letters A-Z and a-z
@@ -83,26 +97,52 @@ while True:
                 for i in range(newpassword_length):                                                                    #This checks the amount of letters chosen by the user.
                     randomchar = secrets.choice(newpassword_characters)                                                #This selects a random set of characters out of the ones defined above.
                     newpassword.append(randomchar)
-                print("Secure password suggestion: " + "".join(newpassword))                                           #The check will be performed here once that feature is implemented.
-                input("Press RETURN to go back to the menu.")
+                print(t("password_suggestion"))                                                                        #A check of the new password will be performed here once that feature is implemented.
+                print("".join(newpassword)) 
+                input(t("press_return_menu"))
         except:
-            input("Invalid input! Press RETURN to continue.")
+            input(t("invalid_input"))
             
-    elif userchoice=="L" or userchoice=="l":
-        print("""       SELECT A LANGUAGE
-              Svenska
-              English
-              Deutsch""")                                                                                              #Language choice logic will be placed here. Not yet implemented.
-        input("Press RETURN to go back.")
+    elif userchoice=="L" or userchoice=="l" or userchoice == "S" or userchoice =="s":
+        print(t("language_menu"))
+        lang_choice = input()
+        if lang_choice == "en":
+            translator = Localisation("en")
+            t = translator.t
+        elif lang_choice == "sv":
+            translator = Localisation("sv")
+            t = translator.t
+        elif lang_choice == "fi":
+            translator = Localisation("fi")
+            t = translator.t
+        elif lang_choice == "de":
+            translator = Localisation("de")
+            t = translator.t
+        else:
+            input(t("invalid_input"))
+
+        input(t("press_return_menu"))
 
     elif userchoice=="X" or userchoice=="x":
-        break                                                                                                          #This exits the program.
+        break                                                                                                          #This stops the program. User might have to close the console themselves.
             
-    else: print ("Invalid choice! Please press one of the keys asked.")                                                #Prevents the program from crashing.
+    else: print (t("invalid_choice"))                                                                                  #Prevents the program from crashing due to invalid input.
 
-            #Add the different features here
-            #Make a log file!
-            
+            #Changelog 02-01-2026 (v1.0)
+            #Issues with password length have been resolved. Now passwords generate as they should.
+            #Finnish language file added. Thank you Vilho / saabismi for writing the translation!  
+            #Welcome sign in the menu now translates correctly, it was still hard-coded before.     
+    
+
+            #Changelog 02-01-2026 (v0.9e - Localisation branch)
+            #Added Localisation class.
+            #Added three language JSON files that the script reads from.
+            #Added a language selection which is accessed from the menu.
+            #These changes have lead to issues with the password length and a few text formatting errors.
+
+            #Changelog 02-01-2026 (v0.8b - HIPB branch)
+            #Importing "getpass" to hide the existing password input.
+
             #Changelog 01-01-2026 (v0.7b - HIPB branch)
             #Added the "check_password" function to check an existing password against HaveIBeenPwnd's API using a SHA-1 hash.
             #Importing standard libraries which should work on different operating systems. Tested on Windows.
@@ -123,4 +163,5 @@ while True:
             #newpassword - The new password that the program generates, with certain characteristics.
             #newpassword_length - Integer defining the amount of characters in the generated new password. User chooses this.
             #newpassword_characters - String containing the character set that gets used in the password. Defined by the imported ASCII character set; A-Z, a-z, 0-9, punctuation.
+            #lang_choice - Defines which JSON file to read in order to print different languages. The value is given to the Localisation class.
             
